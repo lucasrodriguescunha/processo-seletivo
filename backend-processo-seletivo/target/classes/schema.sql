91e3aa606925ebd294bfb5c06a891f2f27d966bb
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-10-01 09:38:01 -0300

    Adiciona as pastas frontend e backend

--- backend-processo-seletivo/target/classes/schema.sql
+++ backend-processo-seletivo/target/classes/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE projetos (
-                         id SERIAL PRIMARY KEY,
-                         nome VARCHAR(255) NOT NULL,
-                         descricao TEXT NOT NULL,
-                         status VARCHAR(50) DEFAULT NULL, -- Permite que o status seja NULL
-                         data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                          id SERIAL PRIMARY KEY,  -- Identificador único e autoincrementável para cada projeto
+                          nome VARCHAR(255) NOT NULL,  -- Nome do projeto é obrigatório e limitado a 255 caracteres
+                          descricao TEXT NOT NULL,  -- Descrição detalhada do projeto, campo obrigatório
+                          status VARCHAR(50) DEFAULT NULL,  -- Status do projeto opcional, permite valores nulos
+                          data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Define a data de criação com valor padrão sendo o momento da inserção
 );
 
 CREATE TABLE tarefas (
-                        id SERIAL PRIMARY KEY,
-                        nome VARCHAR(255) NOT NULL,
-                        descricao TEXT NOT NULL,
-                        concluida BOOLEAN DEFAULT FALSE,
-                        status VARCHAR(50) DEFAULT NULL, -- Permite que o status seja NULL
-                        projeto_id BIGINT,
-                        FOREIGN KEY (projeto_id) REFERENCES projetos(id) ON DELETE CASCADE
+                         id SERIAL PRIMARY KEY,  -- Identificador único e autoincrementável para cada tarefa
+                         nome VARCHAR(255) NOT NULL,  -- Nome da tarefa é obrigatório e limitado a 255 caracteres
+                         descricao TEXT NOT NULL,  -- Descrição detalhada da tarefa, campo obrigatório
+                         concluida BOOLEAN DEFAULT FALSE,  -- Indica se a tarefa foi concluída ou não, com valor padrão sendo falso
+                         status VARCHAR(50) DEFAULT NULL,  -- Status da tarefa opcional, permite valores nulos
+                         projeto_id BIGINT,  -- Relaciona a tarefa a um projeto específico
+                         FOREIGN KEY (projeto_id) REFERENCES projetos(id) ON DELETE CASCADE  -- Chave estrangeira ligando à tabela projetos; ao excluir um projeto, as tarefas associadas também serão excluídas
 );
